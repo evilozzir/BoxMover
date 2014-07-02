7fb6b5ec9d72fd56cd4bbeea46abba74c61a059f
Gonzalo Rizzo <evilozzir@gmail.com>
2014-07-02 19:03:30 -0300

    Adding Main class

--- src/eoz/boxMover/Main.java
+++ src/eoz/boxMover/Main.java
@@ -1,4 +1,25 @@
 package eoz.boxMover;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
 
 public class Main {
+    public static void main(String[] args){
+        try{
+            Display.setDisplayMode(new DisplayMode(1024,768));
+            Display.setTitle("Box Mover");
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+        glMatrixMode(GL_PROJECTION);
+        glOrtho(0, 640, 480, 0, 1, -1);
+        glMatrixMode(GL_MODELVIEW);
+        while (!Display.isCloseRequested()){
+            Display.update();
+            Display.sync(60);
+        }
+
+    }
 }
